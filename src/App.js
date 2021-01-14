37dda6a36878eae69dddfd4707ba87d920417a90
Kunalpaul12 <kunalletstalk@gmail.com>
2021-01-14 10:30:55 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {NavbarBlack} from './component/nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{flex: 1}}>
+      {NavbarBlack()}
+      <div>
+        <p>hii let tset this now ?</p>
+      </div>
     </div>
   );
 }
---
+++ src/component/nav/index.js
@@ -0,0 +1,9 @@
+import Navbar from 'react-bootstrap/Navbar';
+
+export const NavbarBlack = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>Deera</Navbar.Brand>
+    </Navbar>
+  );
+};
